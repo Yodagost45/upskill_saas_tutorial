af9c92fdbbfa58f95fd525a8d40cda4327976a54
Jonathan Hall <jonathan78564@gmail.com>
2019-05-29 03:27:13 +0000

    Added contact form mailer file

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,11 @@
+class ContactMailer < ActionMailer::Base
+  deafult to: 'jonathan78564@gmail.com'
+  
+  def contact_email(name, email, body)
+    @name = name
+    @email = email
+    @body = body
+    
+    mail(from: email, subject: 'Contact Form Message')
+  end
+end
